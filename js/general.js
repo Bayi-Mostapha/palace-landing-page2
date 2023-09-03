35a64734a8c58b8939b28480b3062305c03a753d
skwizo <bayimostapha@gmail.com>
2023-09-03 13:00:36 +0100

    switched navbar stuff to general.css + general.js

---
+++ js/general.js
@@ -0,0 +1,72 @@
+const root = document.documentElement;
+const loader = document.querySelector('.loader');
+
+window.onload = function () {
+    loader.style.display = 'none';
+    document.body.classList.remove('hide-overflow');
+
+    // ****************************** Intersection observer
+
+    const faders = document.querySelectorAll('.fade-in');
+
+    const appearOptions = {
+        threshold: 0.2
+    };
+
+    const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            }
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        });
+    }, appearOptions);
+
+    if (faders) {
+        faders.forEach(fader => {
+            appearOnScroll.observe(fader);
+        });
+    }
+
+    // *********************************************
+
+    const mobileNavBtn = document.querySelector('.lp-nav-btn');
+    const mobileNav = document.querySelector('.lp-mobile-nav');
+    if (mobileNav && mobileNavBtn) {
+        mobileNavBtn.addEventListener('click', () => {
+            if (!mobileNav.classList.contains('shown')) {
+                mobileNav.classList.add('shown');
+                mobileNavBtn.classList.add('navbar-shown');
+                document.body.classList.add('hide-overflow');
+            } else {
+                mobileNav.classList.remove('shown');
+                mobileNavBtn.classList.remove('navbar-shown');
+                document.body.classList.remove('hide-overflow');
+            }
+        });
+    }
+
+    // resize event 
+
+    window.addEventListener('resize', () => {
+        if (mobileNav && mobileNavBtn) {
+            if (window.innerWidth > 750 && mobileNav.classList.contains('shown')) {
+                mobileNav.classList.remove('shown');
+                mobileNavBtn.classList.remove('navbar-shown');
+                document.body.classList.remove('hide-overflow');
+            }
+        }
+    });
+
+    // scroll event
+
+    window.addEventListener('scroll', function () {
+        if (window.scrollY === 0 && faders) {
+            faders.forEach(fader => {
+                fader.classList.remove('appear');
+                appearOnScroll.observe(fader);
+            });
+        }
+    });
+};
--- js/landing-page.js
+++ js/landing-page.js
@@ -1,87 +1,18 @@
-const root = document.documentElement;
-const loader = document.querySelector('.loader');
+// ********************************************* Hero height
+function updateHeights() {
+    const section1_left = document.querySelector('.section1 .left').offsetHeight;
+    const section1_right = document.querySelector('.section1 .right .picture').offsetHeight;
+    const small_pic = document.querySelector('.section1 .right .small-picture').offsetHeight;
 
-window.onload = function () {
-    loader.style.display = 'none';
-    document.body.classList.remove('hide-overflow');
-
-    // ****************************** Intersection observer
-
-    const faders = document.querySelectorAll('.fade-in');
-
-    const appearOptions = {
-        threshold: 0.2
-    };
-
-    const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
-        entries.forEach(entry => {
-            if (!entry.isIntersecting) {
-                return;
-            }
-            entry.target.classList.add('appear');
-            appearOnScroll.unobserve(entry.target);
-        });
-    }, appearOptions);
-
-    faders.forEach(fader => {
-        appearOnScroll.observe(fader);
-    });
-
-    // *********************************************
-
-    const mobileNavBtn = document.querySelector('.lp-nav-btn');
-    const mobileNav = document.querySelector('.lp-mobile-nav');
-    mobileNavBtn.addEventListener('click', () => {
-        if (!mobileNav.classList.contains('shown')) {
-            mobileNav.classList.add('shown');
-            mobileNavBtn.classList.add('navbar-shown');
-            document.body.classList.add('hide-overflow');
-        } else {
-            mobileNav.classList.remove('shown');
-            mobileNavBtn.classList.remove('navbar-shown');
-            document.body.classList.remove('hide-overflow');
-        }
-    });
-
-    // ********************************************* Hero height
-
-    function updateHeights() {
-        const section1_left = document.querySelector('.section1 .left').offsetHeight;
-        const section1_right = document.querySelector('.section1 .right .picture').offsetHeight;
-        const small_pic = document.querySelector('.section1 .right .small-picture').offsetHeight;
-        const h2Element = document.querySelector('.section1 h2');
-
-        if (window.innerWidth > 900) {
-            root.style.setProperty('--section1-height', `${Math.max(section1_left, section1_right) + small_pic / 4}px`);
-        } else {
-            root.style.setProperty('--section1-height', `${section1_left + section1_right + small_pic + 150}px`);
-        }
+    if (window.innerWidth > 900) {
+        root.style.setProperty('--section1-height', `${Math.max(section1_left, section1_right) + small_pic / 4}px`);
+    } else {
+        root.style.setProperty('--section1-height', `${section1_left + section1_right + small_pic + 150}px`);
     }
+}
+updateHeights();
 
+// resize event 
+window.addEventListener('resize', () => {
     updateHeights();
-
-    // *****************************
-
-    // resize event 
-
-    window.addEventListener('resize', () => {
-        updateHeights();
-
-        if (window.innerWidth > 750 && mobileNav.classList.contains('shown')) {
-            mobileNav.classList.remove('shown');
-            mobileNavBtn.classList.remove('navbar-shown');
-            document.body.classList.remove('hide-overflow');
-        }
-    });
-
-    // scroll event
-
-    window.addEventListener('scroll', function () {
-        if (window.scrollY === 0) {
-            faders.forEach(fader => {
-                fader.classList.remove('appear');
-                appearOnScroll.observe(fader);
-            });
-        }
-    });
-};
+});
