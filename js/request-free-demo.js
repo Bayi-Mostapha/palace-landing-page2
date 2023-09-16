27bd465fc7c397627e1365fbfc5a5e0c511e7bb4
skwizo <bayimostapha@gmail.com>
2023-09-16 12:38:55 +0100

    request demo js done

--- js/request-free-demo.js
+++ js/request-free-demo.js
@@ -1,16 +1,22 @@
-// ********************************************
 function updateHeights() {
-    const section = document.querySelector('.main .top .right').offsetHeight;
-    const img1 = document.querySelector('.main .top .right .floating-img1').offsetHeight;
-    const img2 = document.querySelector('.main .top .right .floating-img2').offsetHeight;
+    const img1Element = document.querySelector('.main .top .right .floating-img1');
+    let img1;
+    if (img1Element)
+        img1 = img1Element.offsetHeight;
+    const img2Element = document.querySelector('.main .top .right .floating-img2');
+    let img2;
+    if (img2Element)
+        img2 = img2Element.offsetHeight;
 
     if (window.innerWidth < 650) {
         root.style.setProperty('--top-height', `${Math.max(img1, img2)}px`);
     }
 }
+
 updateHeights();
 
 // resize event 
+
 window.addEventListener('resize', () => {
     updateHeights();
 });
